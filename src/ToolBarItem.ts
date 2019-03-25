7ec61ef80456899e63fd2d76158cc705c9bb0fde
vanely <vanelys94@gmail.com>
2019-03-24 20:27:45 -0400

    toolbar class

--- src/ToolBarItem.ts
+++ src/ToolBarItem.ts
@@ -1,18 +1,63 @@
 module toolBar {
 
   export class ToolBarItem {
-    item: ToolBarItem;
     private __el: HTMLElement;
 
     constructor(itemElement: HTMLElement) {
       this.__el = itemElement;
       this.__el.addEventListener('click', (e) => {
-        //TODO ad call to activate or deactivate item
+        this.toggleActiveState();
       });
     }
 
+    private addClass(className: string) {
+      this.__el.classList.add(className);
+    }
+    
+    private removeClass(className: string) {
+      this.__el.classList.remove(className);
+    }
+
     get enabled() {
+      return !this.__el.classList.contains('disabled');
+    }
+
+    set enabled(value: boolean) {
+      let currentValue: boolean = this.enabled;
       
+      if(currentValue === value) {
+        return;
+      }
+      if(value) {
+        this.removeClass('disabled');
+      } else {
+        this.addClass('disabled');
+      }
+    }
+
+    get activated() {
+      return this.__el.classList.contains('active');
+    }
+
+    set activated(value: boolean) {
+      let currentValue: boolean = this.enabled;
+
+      if(currentValue === value || !this.enabled) {
+        return;
+      }
+      if(value) {
+        this.addClass('activate');
+      } else {
+        this.removeClass('activate');
+      } 
+    }
+
+    toggleActiveState() {
+      this.activated = !this.activated;
+    }
+
+    removeFrom(parentElement: HTMLElement) {
+      parentElement.removeChild(this.__el);
     }
   }
 
